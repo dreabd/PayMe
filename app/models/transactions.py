a2ccdf0877ed2c80923e9a7c1375e9b6cbbd0688
Andre <andreabad17@gmail.com>
2023-06-13 02:38:44 -0700

    more database stuff

--- app/models/transactions.py
+++ app/models/transactions.py
@@ -24,8 +24,8 @@ class Transaction(db.Model):
         db.Integer, db.ForeignKey(add_prefix_for_prod("categories.id")), nullable=False
     )
 
-    requester  = db.relationship("User", foreign_keys=[requester_id])
-    payer = db.relationship("User", foreign_keys=[payer_id])
+    requester  = db.relationship("User", foreign_keys='[requester_id]')
+    payer = db.relationship("User", foreign_keys='[payer_id]')
     category = db.relationship("Category", back_populates="transaction")
 
     def to_dict(self):
