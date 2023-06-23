956a2326a187ff636ff0bf1b81b04a40aad9eaec
Andre Abad <andreabad17@gmail.com>
2023-06-23 09:50:14 -0700

    that was an interesting experince

--- react-app/src/components/UserLandingPage/components/UserPage/index.js
+++ react-app/src/components/UserLandingPage/components/UserPage/index.js
@@ -4,20 +4,33 @@ import { useHistory, useParams } from "react-router-dom/cjs/react-router-dom.min
 
 function UserPage() {
   const { id } = useParams()
-  console.log(id)
+  const dispatch = useDispatch()
+
+
+
   const user = useSelector(state => state.session.user)
 
-  console.log(user.id === id)
+  useEffect(()=>{
+    // if it is a friend dipatch a thunk that gets the friends' public trans and trans between the user and the friend
+    // if it is a random person just show transactions that are public 
+    // if it is the user then dispatch a thunk that gets all the user's completed transaction 
+  },[dispatch])
+
+
   if (id == user.id) {
     return (
-      <h1> yes</h1>
+      <div>
+        {/* Here I want to show all of their transactions */}
+        {/* If it is the user's peronsal page then want to show the budget / spending habits */}
+      </div>
     )
   }
   
   return (
-    <h1>
-      No
-    </h1>
+    <div> 
+      {/* Here if want to show the name of the perosn, if they are a friend, their transactions, and the transaction between the current user and the friend */}
+      {/* If they are not a friend then show not a friend and only the public transactions. */}
+    </div>
   )
 }
 
